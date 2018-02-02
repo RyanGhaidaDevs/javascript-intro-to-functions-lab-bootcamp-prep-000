4b3fa7cb1dffeda283e5318a30ec56f27e1541fb
Ryan Ghaida <ryaboug@gmail.com>
2018-02-02 22:05:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function sayHiToGrandma (string){
   else if (string === "I love you, Grandma." )
   return " I love you, too."
   
-  else {
+  else if (string===string.toLowerCase)
     return "I can't hear you! "
   }
   
